2b7c2fd3bdce22911df5b1d8b0ccd5d645ac6ead
greatericontop <greateric@protonmail.com>
2023-03-12 12:59:31 -0400

    base commit

---
+++ src/main/java/io/github/greatericontop/greatimpostor/GreatImpostorMain.java
@@ -0,0 +1,13 @@
+package io.github.greatericontop.greatimpostor;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class GreatImpostorMain extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+
+        this.getLogger().info("GreatImpostor finished setting up!");
+    }
+
+}
