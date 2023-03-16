8a1c242977701e2ab2f4ad0d9282cb5e1f951684
greatericontop <greateric@protonmail.com>
2023-03-15 21:02:56 -0400

    visuals

--- src/main/java/io/github/greatericontop/greatimpostor/core/CrewmateProfile.java
+++ src/main/java/io/github/greatericontop/greatimpostor/core/CrewmateProfile.java
@@ -1,10 +1,7 @@
 package io.github.greatericontop.greatimpostor.core;
 
-import net.kyori.adventure.text.Component;
 import org.bukkit.entity.Player;
 import org.bukkit.inventory.Inventory;
-import org.bukkit.inventory.ItemStack;
-import org.bukkit.inventory.meta.ItemMeta;
 
 public class CrewmateProfile extends PlayerProfile {
 
@@ -16,23 +13,9 @@ public class CrewmateProfile extends PlayerProfile {
         Inventory inv = this.getPlayer().getInventory();
         inv.clear();
 
-        ItemStack firstTask = new ItemStack(tasks.get(0).getDisplayMaterial(subtasksCompletedPerTask[0]), 1);
-        ItemMeta im1 = firstTask.getItemMeta();
-        im1.displayName(Component.text(tasks.get(0).getDisplayName()));
-        firstTask.setItemMeta(im1);
-        inv.setItem(0, firstTask);
-
-        ItemStack secondTask = new ItemStack(tasks.get(1).getDisplayMaterial(subtasksCompletedPerTask[1]), 1);
-        ItemMeta im2 = secondTask.getItemMeta();
-        im2.displayName(Component.text(tasks.get(1).getDisplayName()));
-        secondTask.setItemMeta(im2);
-        inv.setItem(1, secondTask);
-
-        ItemStack thirdTask = new ItemStack(tasks.get(2).getDisplayMaterial(subtasksCompletedPerTask[2]), 1);
-        ItemMeta im3 = thirdTask.getItemMeta();
-        im3.displayName(Component.text(tasks.get(2).getDisplayName()));
-        thirdTask.setItemMeta(im3);
-        inv.setItem(2, thirdTask);
+        inv.setItem(0, tasks.get(0).getDisplayItemStack(subtasksCompletedPerTask[0], "§7A - "));
+        inv.setItem(1, tasks.get(1).getDisplayItemStack(subtasksCompletedPerTask[1], "§7B - "));
+        inv.setItem(2, tasks.get(2).getDisplayItemStack(subtasksCompletedPerTask[2], "§7C - "));
 
     }
 
--- src/main/java/io/github/greatericontop/greatimpostor/task/Subtask.java
+++ src/main/java/io/github/greatericontop/greatimpostor/task/Subtask.java
@@ -1,36 +1,39 @@
 package io.github.greatericontop.greatimpostor.task;
 
+import net.kyori.adventure.text.Component;
 import org.bukkit.Material;
+import org.bukkit.inventory.ItemStack;
+import org.bukkit.inventory.meta.ItemMeta;
 
 public enum Subtask {
 
     // To prevent a cyclic dependency between Subtask and Task this needs to be a string that gets put into a TaskType at runtime
 
-    WIRING_ELECTRICAL("WIRING"),
-    WIRING_STORAGE("WIRING"),
-    WIRING_ADMIN("WIRING"),
-    WIRING_NAVIGATION("WIRING"),
-    WIRING_CAFETERIA("WIRING"),
-    WIRING_SECURITY("WIRING"),
+    WIRING_ELECTRICAL("WIRING", "§eElectrical: Fix Wiring"),
+    WIRING_STORAGE("WIRING", "§eStorage: Fix Wiring"),
+    WIRING_ADMIN("WIRING", "§eAdmin: Fix Wiring"),
+    WIRING_NAVIGATION("WIRING", "§eNavigation: Fix Wiring"),
+    WIRING_CAFETERIA("WIRING", "§eCafeteria: Fix Wiring"),
+    WIRING_SECURITY("WIRING", "§eSecurity: Fix Wiring"),
 
-    REDIRECT_POWER("REDIRECT_ACCEPT_POWER"),
+    REDIRECT_POWER("REDIRECT_ACCEPT_POWER", "§eElectrical: Redirect Power"),
 
-    ACCEPT_POWER_COMMUNICATIONS("REDIRECT_ACCEPT_POWER"),
-    ACCEPT_POWER_LOWER_ENGINE("REDIRECT_ACCEPT_POWER"),
-    ACCEPT_POWER_UPPER_ENGINE("REDIRECT_ACCEPT_POWER"),
-    ACCEPT_POWER_NAVIGATION("REDIRECT_ACCEPT_POWER"),
-    ACCEPT_POWER_OXYGEN("REDIRECT_ACCEPT_POWER"),
-    ACCEPT_POWER_SECURITY("REDIRECT_ACCEPT_POWER"),
-    ACCEPT_POWER_SHIELDS("REDIRECT_ACCEPT_POWER"),
-    ACCEPT_POWER_WEAPONS("REDIRECT_ACCEPT_POWER"),
+    ACCEPT_POWER_COMMUNICATIONS("REDIRECT_ACCEPT_POWER", "§eCommunications: Accept Redirected Power"),
+    ACCEPT_POWER_LOWER_ENGINE("REDIRECT_ACCEPT_POWER", "§eLower Engine: Accept Redirected Power"),
+    ACCEPT_POWER_UPPER_ENGINE("REDIRECT_ACCEPT_POWER", "§eUpper Engine: Accept Redirected Power"),
+    ACCEPT_POWER_NAVIGATION("REDIRECT_ACCEPT_POWER", "§eNavigation: Accept Redirected Power"),
+    ACCEPT_POWER_OXYGEN("REDIRECT_ACCEPT_POWER", "§eOxygen: Accept Redirected Power"),
+    ACCEPT_POWER_SECURITY("REDIRECT_ACCEPT_POWER", "§eSecurity: Accept Redirected Power"),
+    ACCEPT_POWER_SHIELDS("REDIRECT_ACCEPT_POWER", "§eShields: Accept Redirected Power"),
+    ACCEPT_POWER_WEAPONS("REDIRECT_ACCEPT_POWER", "§eWeapons: Accept Redirected Power"),
 
-    DOWNLOAD_DATA_CAFETERIA("DOWNLOAD_UPLOAD_DATA"),
-    DOWNLOAD_DATA_COMMUNICATIONS("DOWNLOAD_UPLOAD_DATA"),
-    DOWNLOAD_DATA_ELECTRICAL("DOWNLOAD_UPLOAD_DATA"),
-    DOWNLOAD_DATA_NAVIGATION("DOWNLOAD_UPLOAD_DATA"),
-    DOWNLOAD_DATA_WEAPONS("DOWNLOAD_UPLOAD_DATA"),
+    DOWNLOAD_DATA_CAFETERIA("DOWNLOAD_UPLOAD_DATA", "§eCafeteria: Download Data"),
+    DOWNLOAD_DATA_COMMUNICATIONS("DOWNLOAD_UPLOAD_DATA", "§eCommunications: Download Data"),
+    DOWNLOAD_DATA_ELECTRICAL("DOWNLOAD_UPLOAD_DATA", "§eElectrical: Download Data"),
+    DOWNLOAD_DATA_NAVIGATION("DOWNLOAD_UPLOAD_DATA", "§eNavigation: Download Data"),
+    DOWNLOAD_DATA_WEAPONS("DOWNLOAD_UPLOAD_DATA", "§eWeapons: Download Data"),
 
-    UPLOAD_DATA("DOWNLOAD_UPLOAD_DATA"),
+    UPLOAD_DATA("DOWNLOAD_UPLOAD_DATA", "§eAdmin: Upload Data"),
 
     ;
 
@@ -44,20 +47,30 @@ public enum Subtask {
         return displayName;
     }
 
-    Subtask(String fullTask) {
-        this.displayName = this.name(); // TODO: actually get nice names
+    Subtask(String fullTask, String displayName) {
+        this.displayName = displayName;
         this.fullTask = fullTask;
     }
 
-    public Material getDisplayMaterial(int numberCompleted) {
+    public ItemStack getDisplayItemStack(int numberCompleted, String prefix) {
         int total = getFullTask().getRequiredSubtaskCount();
+        Material mat;
+        String displayName;
         if (numberCompleted == total) {
-            return Material.LIME_STAINED_GLASS;
+            mat = Material.LIME_STAINED_GLASS;
+            displayName = "§aAll Done!";
         } else if (numberCompleted == 0) {
-            return Material.RED_STAINED_GLASS;
+            mat = Material.RED_STAINED_GLASS;
+            displayName = String.format("%s §b(%d/%d)", getDisplayName(), numberCompleted+1, total);
         } else {
-            return Material.YELLOW_STAINED_GLASS;
+            mat = Material.YELLOW_STAINED_GLASS;
+            displayName = String.format("%s §b(%d/%d)", getDisplayName(), numberCompleted+1, total);
         }
+        ItemStack taskStack = new ItemStack(mat, 1);
+        ItemMeta im = taskStack.getItemMeta();
+        im.displayName(Component.text(prefix + displayName));
+        taskStack.setItemMeta(im);
+        return taskStack;
     }
 
 }
