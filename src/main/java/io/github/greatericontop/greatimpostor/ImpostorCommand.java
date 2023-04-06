408da23718e9dfdea18112051dc8ce6f5c99c42d
greatericontop <greateric@protonmail.com>
2023-04-05 20:12:21 -0400

    more tutorial

--- src/main/java/io/github/greatericontop/greatimpostor/ImpostorCommand.java
+++ src/main/java/io/github/greatericontop/greatimpostor/ImpostorCommand.java
@@ -31,12 +31,13 @@ public class ImpostorCommand implements CommandExecutor {
                     Component.text("GreatImpostor"),
                     Component.text("§dWelcome to GreatImpostor!\n\n\n§0This is a tutorial to help you start."),
                     // crew
-                    Component.text("§3§lCREWMATE\n\n\n§0Do your tasks!\n\nYou'll have 4 pieces of glass indicating what tasks to do. They'll change color as you complete them."),
+                    Component.text("§3§lCREWMATE\n\n\n§0Do your tasks!\n\nYou'll have 4 pieces of glass indicating what tasks to do. Check their names to see what room to go to and what to do. They'll change color as you complete them."),
                     Component.text("§0Right click signs to interact with them.\n\n§0You can vote during meetings using §6/vote§0."),
                     Component.text("§0Right click the horn to report a body.\n\nYou'll win if everyone finishes their tasks, or if the impostors are found and ejected."),
+                    Component.text("§0If you can't find the room, look around, and there should be directions on the walls."),
                     // impostor
                     Component.text("§c§lIMPOSTOR\n\n\n§0Kill everyone!\n\nAttack a crewmate with the sword to kill them."),
-                    Component.text("§6HOTKEY §0to the TNT to choose a sabotage, and §6HOTKEY §0to the redstone to activate it.\n\n§6SNEAK §0over a vent to use it. Then §6SPACE §0to move around or §6SNEAK §0again to exit."),
+                    Component.text("§6HOTKEY §0to the TNT to choose a sabotage, and §6HOTKEY §0to the redstone to activate it.\n\n§6SNEAK §0over a vent to use it. Then §6JUMP §0to move around or §6SNEAK §0again to exit."),
                     Component.text("§0You'll win if you kill enough crewmates, or if the crew fails to repair a sabotage.")
             ));
             return true;
