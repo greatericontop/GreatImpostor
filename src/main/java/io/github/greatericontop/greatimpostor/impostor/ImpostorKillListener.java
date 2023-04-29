60cfd57d6848143befc87e9664de91e737f36289
greatericontop <greateric@protonmail.com>
2023-04-29 10:35:23 -0400

    place the head of the body

--- src/main/java/io/github/greatericontop/greatimpostor/impostor/ImpostorKillListener.java
+++ src/main/java/io/github/greatericontop/greatimpostor/impostor/ImpostorKillListener.java
@@ -15,8 +15,11 @@ import org.bukkit.event.Listener;
 import org.bukkit.event.entity.EntityDamageByEntityEvent;
 import org.bukkit.inventory.ItemStack;
 import org.bukkit.inventory.meta.LeatherArmorMeta;
+import org.bukkit.inventory.meta.SkullMeta;
 import org.bukkit.persistence.PersistentDataType;
 import org.bukkit.util.EulerAngle;
+
+import javax.annotation.Nullable;
 
 public class ImpostorKillListener implements Listener {
 
@@ -49,6 +52,7 @@ public class ImpostorKillListener implements Listener {
             if (!(event.getEntity() instanceof Player victimPlayer)) {
                 ((Damageable) event.getEntity()).setHealth(0.0);
                 player.sendMessage("§7Note: you're killing a non-player victim");
+                generateDeadBody(event.getEntity().getLocation(), null);
             } else {
                 PlayerProfile victimProfile = plugin.playerProfiles.get(victimPlayer.getUniqueId());
                 if (victimProfile == null) {
@@ -60,26 +64,32 @@ public class ImpostorKillListener implements Listener {
                     return;
                 }
                 victimProfile.die();
+                generateDeadBody(event.getEntity().getLocation(), victimPlayer);
             }
-            generateDeadBody(event.getEntity().getLocation());
         } else {
             event.setCancelled(true);
             player.sendMessage("§cYou can't kill right now!");
         }
     }
 
-    private void generateDeadBody(Location loc) {
+    private void generateDeadBody(Location loc, @Nullable Player deadPlayer) {
         ArmorStand armorStand = loc.getWorld().spawn(loc.add(0.0, -1.15, 0.0), ArmorStand.class);
         armorStand.setInvulnerable(true);
         armorStand.setGravity(false);
         armorStand.setArms(false);
         armorStand.setHeadPose(new EulerAngle(Math.PI/2, 0.0, Math.PI));
         armorStand.setBodyPose(new EulerAngle(Math.PI/2, 0.0, Math.PI));
-        armorStand.getEquipment().setHelmet(new ItemStack(Material.PLAYER_HEAD, 1));
+        ItemStack head = new ItemStack(Material.PLAYER_HEAD, 1);
+        if (deadPlayer != null) {
+            SkullMeta headMeta = (SkullMeta) head.getItemMeta();
+            headMeta.setOwningPlayer(deadPlayer);
+            head.setItemMeta(headMeta);
+        }
+        armorStand.getEquipment().setHelmet(head);
         ItemStack chestplate = new ItemStack(Material.LEATHER_CHESTPLATE, 1);
-        LeatherArmorMeta im = (LeatherArmorMeta) chestplate.getItemMeta();
-        im.setColor(Color.fromRGB(0x990000));
-        chestplate.setItemMeta(im);
+        LeatherArmorMeta armorMeta = (LeatherArmorMeta) chestplate.getItemMeta();
+        armorMeta.setColor(Color.fromRGB(0x990000));
+        chestplate.setItemMeta(armorMeta);
         armorStand.getEquipment().setChestplate(chestplate);
         armorStand.getPersistentDataContainer().set(ImpostorUtil.DEAD_BODY_KEY, PersistentDataType.INTEGER, 1);
     }
