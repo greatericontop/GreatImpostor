1f87cfdda67c6ee5a11d4cce3986c0a59a6c6067
greatericontop <greateric@protonmail.com>
2025-06-13 12:36:25 -0400

    prevent vandalizing item frames

--- src/main/java/io/github/greatericontop/greatimpostor/core/AntiVandalism.java
+++ src/main/java/io/github/greatericontop/greatimpostor/core/AntiVandalism.java
@@ -1,11 +1,13 @@
 package io.github.greatericontop.greatimpostor.core;
 
 import io.github.greatericontop.greatimpostor.GreatImpostorMain;
+import org.bukkit.entity.ItemFrame;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.EventPriority;
 import org.bukkit.event.Listener;
 import org.bukkit.event.block.BlockBreakEvent;
 import org.bukkit.event.block.BlockPlaceEvent;
+import org.bukkit.event.entity.EntityDamageByEntityEvent;
 import org.bukkit.event.inventory.InventoryClickEvent;
 import org.bukkit.event.player.PlayerDropItemEvent;
 
@@ -44,6 +46,13 @@ public class AntiVandalism implements Listener {
     public void onDrop(PlayerDropItemEvent event) {
         // separate event from dropping items *in the inventory*
         if (plugin.playerProfiles.containsKey(event.getPlayer().getUniqueId())) {
+            event.setCancelled(true);
+        }
+    }
+
+    @EventHandler(priority = EventPriority.LOW)
+    public void onAttackItemFrame(EntityDamageByEntityEvent event) {
+        if (event.getEntity() instanceof ItemFrame && plugin.playerProfiles.containsKey(event.getDamager().getUniqueId())) {
             event.setCancelled(true);
         }
     }
